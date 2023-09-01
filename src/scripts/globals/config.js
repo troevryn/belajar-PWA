7f92c866576b586746ea20adcf156c32224089d1
troevryn <135703981+troevryn@users.noreply.github.com>
2023-09-02 00:48:29 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 const CONFIG = {
-  KEY: '647e0a7d38ea448884aa457eaaa88a77',
+  KEY: '',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
